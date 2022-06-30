afe5b0eb59140184b3822d52a87163fd11eed55d
pandora <pandora_tl@yahoo.com>
2022-06-30 12:52:36 +0300

    задача4

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int N, i;
+
+Console.Write("Введите число: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+i = 2;
+
+while(i <= N )
+{
+
+    Console.WriteLine(i);   
+    i = i + 2;
+};
